8343678658ee7345d8ff64b9a077000103931b86
RKVoddapelly <rupesh@guruofjava.com>
2019-01-15 01:46:26 +0530

    190115_01

---
+++ src/test/java/com/guruofjava/learn/jenkins/calculator/CalculatorTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guruofjava.learn.jenkins.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author ArshiaAziz
+ */
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+    
+    @Test
+    public void testSum(){
+        Assert.assertEquals(5, calculator.sum(3, 2));
+    }
+}
