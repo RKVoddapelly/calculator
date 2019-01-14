657afdc3415022fc0b7c858e3c48619aa5b681e0
RKVoddapelly <rupesh@guruofjava.com>
2019-01-15 04:07:10 +0530

    190115_04

--- src/main/java/com/guruofjava/learn/jenkins/calculator/CalculatorApplication.java
+++ src/main/java/com/guruofjava/learn/jenkins/calculator/CalculatorApplication.java
@@ -3,6 +3,7 @@ package com.guruofjava.learn.jenkins.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CalculatorApplication {
 
